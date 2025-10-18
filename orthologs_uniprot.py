9dd41cf5ab7c8d44e80a588e8da8ba7adf1c1d70
Aleksandr Voskoboinikov <109810573+wwoskie@users.noreply.github.com>
2025-10-18 18:32:23 +0000

    Add uniprot

---
+++ orthologs_uniprot.py
@@ -0,0 +1,20 @@
+import requests
+import re
+from dataclasses import dataclass
+
+from config import UNIPROT_SEARCH
+
+
+
+
+
+@dataclass
+class UniprotDatasetsResponse: 
+    pass
+
+def get_orthologs_for_gene_uniprot(symbol, organism_id=9606, size=1):
+    q = f"gene_exact:{symbol} AND organism_id:{organism_id}"
+    params = {"query": q, "format": "json", "size": size}
+    r = requests.get(UNIPROT_SEARCH, params=params, headers={"Accept":"application/json"})
+    r.raise_for_status()
+    return r.json()
