3a41ca442895182c5f689b50f15492a6431bd3ab
Aleksandr Voskoboinikov <109810573+wwoskie@users.noreply.github.com>
2025-10-19 20:06:28 +0000

    Complete pipeline

---
+++ test_run.py
@@ -0,0 +1,19 @@
+import logging
+
+import pandas as pd
+
+from config import PATH_TO_LOGS
+from logging_config import setup_logging
+from pipeline import run_pipeline
+
+df = pd.read_csv("/home/sasha/longevity_project_dev/data/genage_genes/genage_human.csv")
+
+setup_logging(PATH_TO_LOGS)
+logger = logging.getLogger(__name__)
+
+for gene in list(df["symbol"])[11:12]:
+    try:
+        run_pipeline(gene)
+    except Exception as e:
+        logger.error(gene)
+        logger.error(e)
